b10a05193e70a1ae2930da078727aa3bf7086213
Tomek <java-tomek@users.noreply.github.com>
2017-09-24 15:05:43 +0200

    Initial commit

---
+++ src/main/java/com/tomek/Main.java
@@ -0,0 +1,4 @@
+package com.tomek;
+
+public class Main {
+}
---
+++ src/main/java/com/tomek/entity/Movie.java
@@ -0,0 +1,80 @@
+package com.tomek.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "movie")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int movie_id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "year")
+    private Date year;
+
+    @Column(name = "duration")
+    private int duration;
+
+    @Column(name = "director")
+    private String director;
+
+    public int getMovie_id() {
+        return movie_id;
+    }
+
+    public void setMovie_id(int movie_id) {
+        this.movie_id = movie_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getYear() {
+        return year;
+    }
+
+    public void setYear(Date year) {
+        this.year = year;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "movie_id=" + movie_id +
+                ", title='" + title + '\'' +
+                ", year=" + year +
+                ", duration=" + duration +
+                ", director='" + director + '\'' +
+                '}';
+    }
+}
